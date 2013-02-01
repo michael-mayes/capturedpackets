94b7d0d6ac65d81e69be34955b449b7bf19724f6
michaelmayes <michaelmayes@60a21958-6ec5-4e53-b584-9fb3ca76da41>
2013-02-01 13:52:39 +0000

    Add fuller support for IPv6 packets so need to reorganise processing for IPv4 packets

---
+++ trunk/PacketCaptureAnalyser/Ethernet Frame/IP Packet/TCP Packet/TCPPacketConstants.cs
@@ -0,0 +1,76 @@
+﻿//This is free and unencumbered software released into the public domain.
+
+//Anyone is free to copy, modify, publish, use, compile, sell, or
+//distribute this software, either in source code form or as a compiled
+//binary, for any purpose, commercial or non-commercial, and by any
+//means.
+
+//In jurisdictions that recognize copyright laws, the author or authors
+//of this software dedicate any and all copyright interest in the
+//software to the public domain. We make this dedication for the benefit
+//of the public at large and to the detriment of our heirs and
+//successors. We intend this dedication to be an overt act of
+//relinquishment in perpetuity of all present and future rights to this
+//software under copyright law.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+//OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+//ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//OTHER DEALINGS IN THE SOFTWARE.
+
+//For more information, please refer to <http://unlicense.org/>
+
+namespace EthernetFrameNamespace.IPPacketNamespace.TCPPacketNamespace
+{
+    class TCPPacketConstants
+    {
+        //
+        //TCP packet header - 20 bytes
+        //
+
+        //Length
+
+        public const ushort TCPPacketHeaderMinimumLength = 20;
+        public const ushort TCPPacketHeaderMaximumLength = 60;
+
+        //Port number
+
+        public enum TCPPacketPortNumberEnumeration : ushort
+        {
+          DummyValueMin = 0,
+          DummyValueMax = 65535
+        }
+
+        //Flags
+
+        public enum TCPPacketFlags : byte
+        {
+            CWR = 0,
+            ECE,
+            URG,
+            ACK,
+            PSH,
+            RST,
+            SYN,
+            FIN
+        }
+
+        public bool IsTCPPacketFlagSet(System.Byte TheByte, TCPPacketFlags TheTCPPacketFlag)
+        {
+            int Shift = TCPPacketFlags.FIN - TheTCPPacketFlag;
+
+            // Get a single bit in the proper position
+            byte BitMask = (byte)(1 << Shift);
+
+            // Mask out the appropriate bit
+            byte MaskedByte = (byte)(TheByte & BitMask);
+
+            // If masked != 0, then the masked out bit is 1
+            // Otherwise, masked will be 0
+            return (MaskedByte != 0);
+        }
+    }
+}
