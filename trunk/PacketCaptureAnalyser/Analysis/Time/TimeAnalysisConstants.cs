d52ee0f8aa5cf606cf5af7f7d812b25a89380a5f
michaelmayes <michaelmayes@60a21958-6ec5-4e53-b584-9fb3ca76da41>
2013-03-07 18:49:19 +0000

    Add constants for the minimum expected timestamp difference (to allow weeding out of duplicates) and for the expected timestamp difference (in case your time messages are not one second apart) Change to using the same number of value bins for timestamp differences as for time differences to allow for easier comparison Correct comment

--- trunk/PacketCaptureAnalyser/Analysis/Time/TimeAnalysisConstants.cs
+++ trunk/PacketCaptureAnalyser/Analysis/Time/TimeAnalysisConstants.cs
@@ -27,17 +27,23 @@ namespace AnalysisNamespace
 {
     class TimeAnalysisConstants
     {
-        //Latencies
+        //Timestamps
+
+        public const double TimeAnalysisMinTimestampDifference = 0.03; //Milliseconds
+
+        //Timestamp and Time differences
+
+        public const double TimeAnalysisExpectedDifference = 1000.0; //Milliseconds
 
         public const double TimeAnalysisMaxNegativeDifference = -2.0; //Milliseconds
         public const double TimeAnalysisMaxPositiveDifference = 2.0; //Milliseconds
 
         //Value bins
 
-        public const int TimeAnalysisTimestampBinsPerMs = 10; //Ten value bins for every millisecond of latency
+        public const int TimeAnalysisTimestampBinsPerMs = 200; //Two hundred value bins for every millisecond of latency
         public const int TimeAnalysisTimestampNumberOfBins = ((int)(TimeAnalysisMaxPositiveDifference - TimeAnalysisMaxNegativeDifference) * TimeAnalysisTimestampBinsPerMs);
 
-        public const int TimeAnalysisTimeBinsPerMs = 200; //Ten value bins for every millisecond of latency
+        public const int TimeAnalysisTimeBinsPerMs = 200; //Two hundred value bins for every millisecond of latency
         public const int TimeAnalysisTimeNumberOfBins = ((int)(TimeAnalysisMaxPositiveDifference - TimeAnalysisMaxNegativeDifference) * TimeAnalysisTimeBinsPerMs);
     }
 }
