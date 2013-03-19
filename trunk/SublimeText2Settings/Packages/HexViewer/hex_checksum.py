1cd6b96c450f447b0df9e2103d62a90e3d7b8ff6
michaelmayes <michaelmayes@60a21958-6ec5-4e53-b584-9fb3ca76da41>
2013-03-19 19:27:43 +0000

    Configure my settings for Sublime Text 2 so that I can merge and share them across multiple PCs

---
+++ trunk/SublimeText2Settings/Packages/HexViewer/hex_checksum.py
@@ -0,0 +1,329 @@
+'''
+Hex Viewer
+Licensed under MIT
+Copyright (c) 2011 Isaac Muse <isaacmuse@gmail.com>
+'''
+
+import sublime
+import sublime_plugin
+import re
+from hex_common import *
+import threading
+import hashlib
+import zlib
+import sys
+import whirlpool
+import tiger
+import sum_hashes
+
+DEFAULT_CHECKSUM = "md5"
+VALID_HASH = []
+
+active_thread = None
+
+
+def verify_hashes(hashes):
+    global VALID_HASH
+    for item in hashes:
+        module = item.split(":")
+        if len(module) == 2:
+            try:
+                getattr(sys.modules[module[0]], module[1])
+                VALID_HASH.append(module[1])
+            except:
+                print "Hex Viewer: " + module[1] + " hash is not available!"
+        else:
+            try:
+                hashlib.new(item)
+                VALID_HASH.append(item)
+            except:
+                print "Hex Viewer: " + item + " hash is not available!"
+
+
+# Extra hash SSL and ZLIB classes
+class ssl_algorithm(object):
+    __algorithm = None
+    __name = None
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def digest_size(self):
+        return self.__digest_size
+
+    def algorithm(self, name, digest_size, arg):
+        self.__algorithm = hashlib.new(name)
+        self.__name = name
+        self.__digest_size = digest_size
+        self.update(arg)
+
+    def copy(self):
+        return None if self.__algorithm is None else self.__algorithm.copy()
+
+    def digest(self):
+        return None if self.__algorithm is None else self.__algorithm.digest()
+
+    def hexdigest(self):
+        return None if self.__algorithm is None else self.__algorithm.hexdigest()
+
+    def update(self, arg):
+        if self.__algorithm is not None:
+            self.__algorithm.update(arg)
+
+
+class zlib_algorithm(object):
+    __algorithm = None
+    __name = None
+    __digest_size = 0
+    __hash = 0
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def digest_size(self):
+        return self.__digest_size
+
+    def algorithm(self, name, digest_size, start, arg):
+        self.__algorithm = getattr(zlib, name)
+        self.__name = name
+        self.__digest_size
+        self.__hash = start
+        self.update(arg)
+
+    def copy(self):
+        return self
+
+    def digest(self):
+        return None if self.__algorithm is None else self.__hash & 0xffffffff
+
+    def hexdigest(self):
+        return None if self.__algorithm is None else '%08x' % (self.digest())
+
+    def update(self, arg):
+        if self.__algorithm is not None:
+            self.__hash = self.__algorithm(arg, self.__hash)
+
+
+# Additional Hashes
+class md2(ssl_algorithm):
+    def __init__(self, arg=''):
+        self.algorithm('md2', 16, arg)
+
+
+class mdc2(ssl_algorithm):
+    def __init__(self, arg=''):
+        self.algorithm('mdc2', 16, arg)
+
+
+class md4(ssl_algorithm):
+    def __init__(self, arg=''):
+        self.algorithm('md4', 16, arg)
+
+
+class sha(ssl_algorithm):
+    def __init__(self, arg=''):
+        self.algorithm('sha', 20, arg)
+
+
+class ripemd160(ssl_algorithm):
+    def __init__(self, arg=''):
+        self.algorithm('ripemd160', 20, arg)
+
+
+class crc32(zlib_algorithm):
+    def __init__(self, arg=''):
+        self.algorithm('crc32', 4, 0, arg)
+
+
+class adler32(zlib_algorithm):
+    def __init__(self, arg=''):
+        self.algorithm('adler32', 4, 1, arg)
+
+
+# Sublime Text Commands
+class checksum(object):
+    thread = None
+
+    def __init__(self, hash_algorithm=None, data=""):
+        if hash_algorithm is None or not hash_algorithm in VALID_HASH:
+            hash_algorithm = hv_settings.get("hash_algorithm", DEFAULT_CHECKSUM)
+        if not hash_algorithm in VALID_HASH:
+            hash_algorithm = DEFAULT_CHECKSUM
+        self.hash = getattr(hashlib, hash_algorithm)(data)
+        self.name = hash_algorithm
+
+    def update(self, data=""):
+        if isinstance(data, basestring):
+            self.hash.update(data)
+
+    def threaded_update(self, data=[]):
+        if not isinstance(data, basestring):
+            global active_thread
+            self.thread = hash_thread(data, self.hash)
+            self.thread.start()
+            self.chunk_thread()
+            active_thread = self
+
+    def chunk_thread(self):
+        ratio = float(self.thread.chunk) / float(self.thread.chunks)
+        percent = int(ratio * 10)
+        leftover = 10 - percent
+        message = "[" + "-" * percent + ">" + "-" * leftover + ("] %3d%%" % int(ratio * 100)) + " chunks hashed"
+        sublime.status_message(message)
+        if not self.thread.is_alive():
+            if self.thread.abort is True:
+                sublime.status_message("Hash calculation aborted!")
+                sublime.set_timeout(lambda: self.reset_thread(), 500)
+            else:
+                sublime.set_timeout(lambda: self.display(), 500)
+        else:
+            sublime.set_timeout(lambda: self.chunk_thread(), 500)
+
+    def reset_thread(self):
+        self.thread = None
+
+    def display(self, window=None):
+        if window is None:
+            window = sublime.active_window()
+        window.show_input_panel(self.name + ":", str(self.hash.hexdigest()), None, None, None)
+
+
+class hash_thread(threading.Thread):
+    def __init__(self, data, obj):
+        self.hash = False
+        self.data = data
+        self.obj = obj
+        self.chunk = 0
+        self.chunks = len(data)
+        self.abort = False
+        threading.Thread.__init__(self)
+
+    def run(self):
+        for chunk in self.data:
+            self.chunk += 1
+            if self.abort:
+                return
+            else:
+                self.obj.update(chunk)
+
+
+class HashSelectionCommand(sublime_plugin.WindowCommand):
+    algorithm = "md5"
+
+    def has_selections(self):
+        single = False
+        view = self.window.active_view()
+        if view is not None:
+            if len(view.sel()) > 0:
+                single = True
+        return single
+
+    def hash_eval(self, value):
+        if value != -1:
+            self.algorithm = VALID_HASH[value]
+            if self.has_selections():
+                # Initialize hasher and related values
+                data = []
+                view = self.window.active_view()
+                hasher = checksum(self.algorithm)
+                # Walk through all selections breaking up data by lines
+                for sel in view.sel():
+                    lines = view.substr(sel).splitlines(True)
+                    for line in lines:
+                        data.append(''.join(unichr(ord(c)).encode('utf-8') for c in line))
+                hasher.threaded_update(data)
+
+    def run(self):
+        if self.has_selections():
+            self.window.show_quick_panel(VALID_HASH, self.hash_eval)
+
+
+class HashEvalCommand(sublime_plugin.WindowCommand):
+    algorithm = "md5"
+
+    def hash_eval(self, value):
+        data = []
+        hasher = checksum(self.algorithm)
+        lines = value.splitlines(True)
+        for line in lines:
+            data.append(''.join(unichr(ord(c)).encode('utf-8') for c in line))
+        hasher.threaded_update(data)
+
+    def select_hash(self, value):
+        if value != -1:
+            self.algorithm = VALID_HASH[value]
+            self.window.show_input_panel(
+                "hash input:",
+                "",
+                self.hash_eval,
+                None,
+                None
+            )
+
+    def run(self, hash_algorithm=None):
+        self.window.show_quick_panel(VALID_HASH, self.select_hash)
+
+
+class HexChecksumCommand(sublime_plugin.WindowCommand):
+    def is_enabled(self):
+        return is_enabled()
+
+    def run(self, hash_algorithm=None, panel=False):
+        global active_thread
+        if active_thread is not None and active_thread.thread is not None and active_thread.thread.is_alive():
+            active_thread.thread.abort = True
+        else:
+            if not panel:
+                self.get_checksum(hash_algorithm)
+            else:
+                self.window.show_quick_panel(VALID_HASH, self.select_checksum)
+
+    def select_checksum(self, value):
+        if value != -1:
+            self.get_checksum(VALID_HASH[value])
+
+    def get_checksum(self, hash_algorithm=None):
+        view = self.window.active_view()
+        if view is not None:
+            sublime.set_timeout(lambda: sublime.status_message("Checksumming..."), 0)
+            hex_hash = checksum(hash_algorithm)
+            r_buffer = view.split_by_newlines(sublime.Region(0, view.size()))
+            hex_data = []
+            for line in r_buffer:
+                hex_data.append(re.sub(r'[\da-z]{8}:[\s]{2}((?:[\da-z]+[\s]{1})*)\s*\:[\w\W]*', r'\1', view.substr(line)).replace(" ", "").decode("hex"))
+            hex_hash.threaded_update(hex_data)
+
+
+# Compose list of hashes
+verify_hashes(
+    [
+        'md2', 'mdc2', 'md4', 'md5',
+        'sha', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512',
+        'ripemd160',
+        'zlib:crc32', 'zlib:adler32',
+        'whirlpool:whirlpool',
+        'tiger:tiger',
+        'sum_hashes:sum8', 'sum_hashes:sum16', 'sum_hashes:sum24', 'sum_hashes:sum32',
+        'sum_hashes:xor8'
+    ]
+)
+
+#Define extra hash classes as members of hashlib
+hashlib.md2 = md2
+hashlib.mdc2 = mdc2
+hashlib.md4 = md4
+hashlib.sha = sha
+hashlib.ripemd160 = ripemd160
+hashlib.crc32 = crc32
+hashlib.adler32 = adler32
+hashlib.whirlpool = whirlpool.whirlpool
+hashlib.tiger = tiger.tiger
+hashlib.sum8 = sum_hashes.sum8
+hashlib.sum16 = sum_hashes.sum16
+hashlib.sum24 = sum_hashes.sum24
+hashlib.sum32 = sum_hashes.sum32
+hashlib.xor8 = sum_hashes.xor8
---
+++ trunk/SublimeText2Settings/Packages/HexViewer/hex_viewer.py
@@ -0,0 +1,413 @@
+'''
+Hex Viewer
+Licensed under MIT
+Copyright (c) 2011 Isaac Muse <isaacmuse@gmail.com>
+'''
+
+import sublime
+import sublime_plugin
+import struct
+import threading
+from os.path import basename
+from os.path import getsize as get_file_size
+from hex_common import *
+from fnmatch import fnmatch
+
+DEFAULT_BIT_GROUP = 16
+DEFAULT_BYTES_WIDE = 24
+VALID_BITS = [8, 16, 32, 64, 128]
+VALID_BYTES = [8, 10, 16, 24, 32, 48, 64, 128, 256, 512]
+AUTO_OPEN = False
+
+
+class ReadBin(threading.Thread):
+    def __init__(self, file_name, bytes_wide, group_size):
+        self.bytes_wide = bytes_wide
+        self.group_size = group_size
+        self.file_name = file_name
+        self.file_size = get_file_size(file_name)
+        self.read_count = 0
+        self.abort = False
+        self.buffer = False
+        threading.Thread.__init__(self)
+
+    def iterfile(self, maxblocksize=4096):
+        with open(self.file_name, "rb") as bin:
+            # Ensure read block is a multiple of groupsize
+            bytes_wide = self.bytes_wide
+            blocksize = maxblocksize - (maxblocksize % bytes_wide)
+
+            start = 0
+            bytes = bin.read(blocksize)
+            while bytes:
+                outbytes = bytes[start:start + bytes_wide]
+                while outbytes:
+                    yield outbytes
+                    start += bytes_wide
+                    outbytes = bytes[start:start + bytes_wide]
+                start = 0
+                bytes = bin.read(blocksize)
+
+    def run(self):
+        translate_table = ("." * 32) + "".join(chr(c) for c in xrange(32, 127)) + ("." * 129)
+        def_struct = struct.Struct("=" + ("B" * self.bytes_wide))
+        def_template = (("%02x" * self.group_size) + " ") * (self.bytes_wide / self.group_size)
+
+        line = 0
+        b_buffer = []
+        read_count = 0
+        for bytes in self.iterfile():
+            if self.abort:
+                return
+            l_buffer = []
+
+            read_count += self.bytes_wide
+            self.read_count = read_count if read_count < self.file_size else self.file_size
+
+            # Add line number
+            l_buffer.append("%08x:  " % (line * self.bytes_wide))
+
+            try:
+                # Complete line
+                # Convert to decimal value
+                values = def_struct.unpack(bytes)
+
+                # Add hex value
+                l_buffer.append(def_template % values)
+            except struct.error:
+                # Incomplete line
+                # Convert to decimal value
+                values = struct.unpack("=" + ("B" * len(bytes)), bytes)
+
+                # Add hex value
+                remain_group = len(bytes) / self.group_size
+                remain_extra = len(bytes) % self.group_size
+                l_buffer.append(((("%02x" * self.group_size) + " ") * (remain_group) + ("%02x" * remain_extra)) % values)
+
+                # Append printable chars to incomplete line
+                delta = self.bytes_wide - len(bytes)
+                group_space = delta / self.group_size
+                extra_space = (1 if delta % self.group_size else 0)
+
+                l_buffer.append(" " * (group_space + extra_space + delta * 2))
+
+            # Append printable chars
+            l_buffer.append(" :" + bytes.translate(translate_table))
+
+            # Add line to buffer
+            b_buffer.append("".join(l_buffer))
+
+            line += 1
+
+        # Join buffer lines
+        self.buffer = "\n".join(b_buffer)
+
+
+class HexViewerListenerCommand(sublime_plugin.EventListener):
+    open_me = None
+
+    def is_bin_file(self, file_path):
+        match = False
+        patterns = hv_settings.get("auto_open_patterns", [])
+        for pattern in patterns:
+            match |= fnmatch(file_path, pattern)
+            if match:
+                break
+        return match
+
+    def open_bin_file(self, view=None, window=None):
+        open_now = False
+        if view != None and window != None:
+            # Direct open file
+            open_now = True
+        else:
+            # Preview view of file
+            window = sublime.active_window()
+            if window != None:
+                view = window.active_view()
+        # Open bin file in hex viewer
+        if window and view and (open_now or view.file_name() == self.open_me):
+            is_preview = window and view.file_name() not in [file.file_name() for file in window.views()]
+            if is_preview:
+                return
+            view.settings().set("hex_no_auto_open", True)
+            window.run_command('hex_viewer')
+
+    def auto_load(self, view, window, is_preview):
+        file_name = view.file_name()
+        # Make sure we have a file name and that we haven't already processed the view
+        if file_name != None and not view.settings().get("hex_no_auto_open", False):
+            # Make sure the file is specified in our binary file list
+            if self.is_bin_file(file_name):
+                # Handle previw or direct open
+                if is_preview:
+                    self.open_me = file_name
+                    sublime.set_timeout(lambda: self.open_bin_file(), 100)
+                else:
+                    self.open_me = file_name
+                    self.open_bin_file(view, window)
+
+    def on_activated(self, view):
+        # Logic for preview windows
+        if hv_settings.get("auto_open", AUTO_OPEN) and not view.settings().get('is_widget'):
+            window = view.window()
+            is_preview = window and view.file_name() not in [file.file_name() for file in window.views()]
+            if view.settings().get("hex_view_postpone_hexview", True) and not view.is_loading():
+                self.auto_load(view, window, is_preview)
+
+    def on_load(self, view):
+        # Logic for direct open files
+        if hv_settings.get("auto_open", AUTO_OPEN) and not view.settings().get('is_widget'):
+            window = view.window()
+            is_preview = window and view.file_name() not in [file.file_name() for file in window.views()]
+            if window and not is_preview and view.settings().get("hex_view_postpone_hexview", True):
+                self.auto_load(view, window, is_preview)
+
+    def on_pre_save(self, view):
+        # We are saving the file so it will now reference itself
+        # Instead of the original binary file, so reset settings.
+        # Hex output will no longer be able to toggle back
+        # To original file, so open original file along side
+        # Newly saved hex output
+        if view.settings().has("hex_viewer_file_name"):
+            view.window().open_file(view.settings().get("hex_viewer_file_name"))
+            view.set_scratch(False)
+            view.set_read_only(False)
+            view.settings().erase("hex_viewer_bits")
+            view.settings().erase("hex_viewer_bytes")
+            view.settings().erase("hex_viewer_actual_bytes")
+            view.settings().erase("hex_viewer_file_name")
+
+
+class HexViewerCommand(sublime_plugin.WindowCommand):
+    handshake = -1
+    file_name = ""
+    thread = None
+
+    def set_format(self):
+        self.group_size = DEFAULT_BIT_GROUP / BITS_PER_BYTE
+        self.bytes_wide = DEFAULT_BYTES_WIDE
+
+        # Set grouping
+        if self.bits in VALID_BITS:
+            self.group_size = self.bits / BITS_PER_BYTE
+
+        # Set bytes per line
+        if self.bytes in hv_settings.get("valid_bytes_per_line", VALID_BYTES):
+            self.bytes_wide = self.bytes
+
+        # Check if grouping and bytes per line do not align
+        # Round to nearest bytes
+        offset = self.bytes_wide % self.group_size
+        if offset == self.bytes_wide:
+            self.bytes_wide = self.bits / BITS_PER_BYTE
+        elif offset != 0:
+            self.bytes_wide -= offset
+
+    def buffer_init(self, bits, bytes):
+        self.view = self.window.active_view()
+        file_name = None
+        if self.view != None:
+            # Get font settings
+            self.font = hv_settings.get('custom_font', 'None')
+            self.font_size = hv_settings.get('custom_font_size', 0)
+
+            #Get file name
+            file_name = self.view.settings().get("hex_viewer_file_name", self.view.file_name())
+
+            # Get current bit and byte settings from view
+            # Or try and get them from settings file
+            # If none are found, use default
+            current_bits = self.view.settings().get(
+                'hex_viewer_bits',
+                hv_settings.get('group_bytes_by_bits', DEFAULT_BIT_GROUP)
+            )
+            current_bytes = self.view.settings().get(
+                'hex_viewer_bytes',
+                hv_settings.get('bytes_per_line', DEFAULT_BYTES_WIDE)
+            )
+            # Use passed in bit and byte settings if available
+            self.bits = bits if bits != None else int(current_bits)
+            self.bytes = int(bytes) if bytes != None else int(current_bytes)
+            self.set_format()
+        return file_name
+
+    def read_bin(self, file_name):
+        self.abort = False
+        self.current_view = self.view
+        self.thread = ReadBin(file_name, self.bytes_wide, self.group_size)
+        self.thread.start()
+        self.handle_thread()
+
+    def load_hex_view(self):
+        file_name = self.thread.file_name
+        b_buffer = self.thread.buffer
+        self.thread = None
+
+        # Show binary data
+        view = self.window.new_file()
+        view.set_name(basename(file_name) + ".hex")
+        self.window.focus_view(self.view)
+        if self.window.active_view().id() == self.view.id():
+            self.window.run_command("close_file")
+        self.window.focus_view(view)
+
+        # Set syntax
+        view.set_syntax_file("Packages/HexViewer/Hex.tmLanguage")
+
+        # Set font
+        if self.font != 'none':
+            view.settings().set('font_face', self.font)
+        if self.font_size != 0:
+            view.settings().set("font_size", self.font_size)
+
+        # Save hex view settings
+        view.settings().set("hex_viewer_bits", self.bits)
+        view.settings().set("hex_viewer_bytes", self.bytes)
+        view.settings().set("hex_viewer_actual_bytes", self.bytes_wide)
+        view.settings().set("hex_viewer_file_name", file_name)
+        view.settings().set("hex_no_auto_open", True)
+
+        # Show hex content in view; make read only
+        view.set_scratch(True)
+        edit = view.begin_edit()
+        view.sel().clear()
+        view.replace(edit, sublime.Region(0, view.size()), b_buffer)
+        view.end_edit(edit)
+        view.set_read_only(True)
+
+        # Offset past address to first byte
+        view.sel().add(sublime.Region(ADDRESS_OFFSET, ADDRESS_OFFSET))
+        if hv_settings.get("inspector", False) and hv_settings.get("inspector_auto_show", False):
+            view.window().run_command("hex_show_inspector")
+
+    def read_file(self, file_name):
+        if hv_settings.get("inspector", False):
+            self.window.run_command("hex_hide_inspector")
+        view = self.window.open_file(file_name)
+        view.settings().set("hex_no_auto_open", True)
+        self.window.focus_view(self.view)
+        self.window.run_command("close_file")
+        self.window.focus_view(view)
+
+    def reset_thread(self):
+        self.thread = None
+
+    def handle_thread(self):
+        if self.abort == True:
+            self.thread.abort = True
+            sublime.status_message("Hex View aborted!")
+            sublime.set_timeout(lambda: self.reset_thread(), 500)
+            return
+        ratio = float(self.thread.read_count) / float(self.thread.file_size)
+        percent = int(ratio * 10)
+        leftover = 10 - percent
+        message = "[" + "-" * percent + ">" + "-" * leftover + ("] %3d%%" % int(ratio * 100)) + " converted to hex"
+        sublime.status_message(message)
+        if not self.thread.is_alive():
+            sublime.set_timeout(lambda: self.load_hex_view(), 100)
+        else:
+            sublime.set_timeout(lambda: self.handle_thread(), 100)
+
+    def abort_hex_load(self):
+        self.abort = True
+
+    def discard_changes(self, value):
+        if value.strip().lower() == "yes":
+            if self.switch_type == "hex":
+                view = sublime.active_window().active_view()
+                if self.handshake == view.id():
+                    view.set_scratch(True)
+                    self.read_bin(self.file_name)
+                else:
+                    sublime.error_message("Target view is no longer in focus!  Hex view aborted.")
+            else:
+                self.read_file(self.file_name)
+        self.reset()
+
+    def discard_panel(self):
+        self.window.show_input_panel(
+            "Discard Changes? (yes | no):",
+            "no",
+            self.discard_changes,
+            None,
+            self.reset
+        )
+
+    def reset(self):
+        self.handshake = -1
+        self.file_name = ""
+        self.type = None
+
+    def run(self, bits=None, bytes=None):
+        # If thread is active cancel thread
+        if self.thread != None and self.thread.is_alive():
+            self.abort_hex_load()
+            return
+
+        # Init Buffer
+        file_name = self.buffer_init(bits, bytes)
+
+        # Identify view
+        if self.handshake != -1 and self.handshake == self.view.id():
+            self.reset()
+        self.handshake = self.view.id()
+
+        if file_name != None:
+            # Decide whether to read in as a binary file or a traditional file
+            if self.view.settings().has("hex_viewer_file_name"):
+                self.view_type = "hex"
+                if is_hex_dirty(self.view):
+                    self.file_name = file_name
+                    if bits == None and bytes == None:
+                        self.switch_type = "file"
+                    else:
+                        self.switch_type = "hex"
+                    self.discard_panel()
+                else:
+                    if bits == None and bytes == None:
+                        # Switch back to traditional output
+                        self.read_file(file_name)
+                    else:
+                        # Reload hex with new settings
+                        self.read_bin(file_name)
+            else:
+                # We are going to swap out the current file for hex output
+                # So as not to clutter the screen.  Changes need to be saved
+                # Or they will be lost
+                if self.view.is_dirty():
+                    self.file_name = file_name
+                    self.switch_type = "hex"
+                    self.discard_panel()
+                else:
+                    # Switch to hex output
+                    self.read_bin(file_name)
+
+
+class HexViewerOptionsCommand(sublime_plugin.WindowCommand):
+    def set_bits(self, value):
+        if value != -1:
+            self.window.run_command('hex_viewer', {"bits": self.valid_bytes[value]})
+
+    def set_bytes(self, value):
+        if value != -1:
+            self.window.run_command('hex_viewer', {"bytes": self.valid_bytes[value]})
+
+    def is_enabled(self):
+        return is_enabled()
+
+    def run(self, option):
+        self.view = self.window.active_view()
+        file_name = self.view.settings().get("hex_viewer_file_name", self.view.file_name())
+        self.valid_bytes = hv_settings.get("valid_bytes_per_line", VALID_BYTES)
+        if file_name != None:
+            if self.view.settings().has("hex_viewer_file_name"):
+                option_list = []
+                if option == "bits":
+                    for bits in self.valid_bytes:
+                        option_list.append(str(bits) + " bits")
+                    self.window.show_quick_panel(option_list, self.set_bits)
+                elif option == "bytes":
+                    for bytes in self.valid_bytes:
+                        option_list.append(str(bytes) + " bytes")
+                    self.window.show_quick_panel(option_list, self.set_bytes)
