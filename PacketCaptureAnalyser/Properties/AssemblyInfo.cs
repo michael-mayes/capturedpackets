3ea7a1878a55ba4ac55ed14dc3443127a0bdccad
michaelmayes <michael-mayes@users.noreply.github.com>
2013-04-18 17:20:13 +0000

    Provide a proper name and copyright for the application

--- PacketCaptureAnalyser/Properties/AssemblyInfo.cs
+++ PacketCaptureAnalyser/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("Packet Capture Analyser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("Packet Capture Analyser")]
+[assembly: AssemblyCopyright("Public Domain")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
