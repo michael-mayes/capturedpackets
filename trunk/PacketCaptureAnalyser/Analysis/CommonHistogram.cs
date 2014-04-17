15124adacf2744fb6f3ffc499cd681adbabddc01
michaelmayes <michaelmayes@60a21958-6ec5-4e53-b584-9fb3ca76da41>
2014-04-17 18:01:00 +0000

    First attempt to add an SVN header

--- trunk/PacketCaptureAnalyser/Analysis/CommonHistogram.cs
+++ trunk/PacketCaptureAnalyser/Analysis/CommonHistogram.cs
@@ -1,4 +1,6 @@
-﻿//This file is part of the C# Packet Capture application. It is free and
+﻿//$Header$
+
+//This file is part of the C# Packet Capture application. It is free and
 //unencumbered software released into the public domain as detailed in
 //the UNLICENSE file in the top level directory of this distribution
 
