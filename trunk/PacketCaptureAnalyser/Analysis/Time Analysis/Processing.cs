0fc9de3f6462c0a28c7ff071efbf4fc47436df6e
michaelmayes <michaelmayes@60a21958-6ec5-4e53-b584-9fb3ca76da41>
2014-04-17 18:18:06 +0000

    Revisions to SVN headers

--- trunk/PacketCaptureAnalyser/Analysis/Time Analysis/Processing.cs
+++ trunk/PacketCaptureAnalyser/Analysis/Time Analysis/Processing.cs
@@ -1,14 +1,11 @@
-﻿//$Header: https://svn.code.sf.net/p/capturedpackets/code/trunk/PacketCaptureAnalyser/Analysis/Time%20Analysis/Processing.cs 212 2014-04-17 18:01:00Z michaelmayes $
-
 //This file is part of the C# Packet Capture application. It is free and
 //unencumbered software released into the public domain as detailed in
 //the UNLICENSE file in the top level directory of this distribution
 
-//SVN revision information:
-//File:    : $URL$
-//Revision : $Revision$
-//Date     : $Date$
-//Author   : $Author$
+//$URL      : https://svn.code.sf.net/p/capturedpackets/code/trunk/PacketCaptureAnalyser/Analysis/Time%20Analysis/Processing.cs $
+//$Revision : 214 $
+//$Date     : 2014-04-17 19:11:24 +0100 (Thu, 17 Apr 2014) $
+//$Author   : michaelmayes $
 
 namespace Analysis.TimeAnalysis
 {
