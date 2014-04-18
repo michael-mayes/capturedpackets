6dcda0e020e74799e3bb45445816b8396252b597
michaelmayes <michaelmayes@60a21958-6ec5-4e53-b584-9fb3ca76da41>
2014-04-18 12:02:34 +0000

    Revisions to SVN headers

--- trunk/PacketCaptureAnalyser/Properties/AssemblyInfo.cs
+++ trunk/PacketCaptureAnalyser/Properties/AssemblyInfo.cs
@@ -1,12 +1,8 @@
+//$Id$
+
 //This file is part of the C# Packet Capture application. It is free and
 //unencumbered software released into the public domain as detailed in
 //the UNLICENSE file in the top level directory of this distribution
-
-//$Id$
-//$URL$
-//$Revision$
-//$Date$
-//$Author$
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
