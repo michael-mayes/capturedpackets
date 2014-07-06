09e0d2f8399033c5d019d24116cdc1d79c343297
michaelmayes <michaelmayes@60a21958-6ec5-4e53-b584-9fb3ca76da41>
2014-07-06 23:05:41 +0000

    Correct minor capitalisation glitch in the standard file header text

--- trunk/PacketCaptureAnalyser/Ethernet Frame/IP Packet/IPv4 Packet/ICMPv4 Packet/Processing.cs
+++ trunk/PacketCaptureAnalyser/Ethernet Frame/IP Packet/IPv4 Packet/ICMPv4 Packet/Processing.cs
@@ -6,7 +6,7 @@
 
 // This file is part of the C# Packet Capture application. It is free and
 // unencumbered software released into the public domain as detailed in
-// The UNLICENSE file in the top level directory of this distribution
+// the UNLICENSE file in the top level directory of this distribution
 
 namespace EthernetFrame.IPPacket.IPv4Packet.ICMPv4Packet
 {
