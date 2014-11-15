bb1cbd2c49d6ee288fd2713ac22c2d98463b97e6
michaelmayes <michael-mayes@users.noreply.github.com>
2014-11-15 21:07:11 +0000

    Replace switch statements using "magic strings" for cases with LINQ lookups into static string arrays - simplifies adding new file extensions

--- PacketCaptureAnalyser/MainWindowFormConstants.cs
+++ PacketCaptureAnalyser/MainWindowFormConstants.cs
@@ -16,6 +16,21 @@ namespace PacketCaptureAnalyser
     public class MainWindowFormConstants
     {
         /// <summary>
+        /// The expected set of fixed extensions for a PCAP Next Generation packet capture
+        /// </summary>
+        private static string[] theExpectedPCAPNextGenerationPacketCaptureFileExtensions = { ".pcapng", ".ntar" };
+
+        /// <summary>
+        /// The expected set of fixed extensions for a PCAP packet capture
+        /// </summary>
+        private static string[] theExpectedPCAPPacketCaptureFileExtensions = { ".pcap", ".libpcap", ".cap" };
+
+        /// <summary>
+        /// The expected set of fixed extensions for an NA Sniffer (DOS) packet capture
+        /// </summary>
+        private static string[] theExpectedNASnifferDOSPacketCaptureFileExtensions = { ".enc" };
+
+        /// <summary>
         /// Enumerated list of the types of packet captures supported by the main window form
         /// </summary>
         public enum PacketCaptureType
@@ -44,6 +59,39 @@ namespace PacketCaptureAnalyser
             /// Unknown value for type of packet capture
             /// </summary>
             Unknown = 4
+        }
+
+        /// <summary>
+        /// Gets the expected set of fixed extensions for a PCAP Next Generation packet capture
+        /// </summary>
+        public static string[] ExpectedPCAPNextGenerationPacketCaptureFileExtensions
+        {
+            get
+            {
+                return theExpectedPCAPNextGenerationPacketCaptureFileExtensions;
+            }
+        }
+
+        /// <summary>
+        /// Gets the expected set of fixed extensions for a PCAP packet capture
+        /// </summary>
+        public static string[] ExpectedPCAPPacketCaptureFileExtensions
+        {
+            get
+            {
+                return theExpectedPCAPPacketCaptureFileExtensions;
+            }
+        }
+
+        /// <summary>
+        /// Gets the expected set of fixed extensions for an NA Sniffer (DOS) packet capture
+        /// </summary>
+        public static string[] ExpectedNASnifferDOSPacketCaptureFileExtensions
+        {
+            get
+            {
+                return theExpectedNASnifferDOSPacketCaptureFileExtensions;
+            }
         }
     }
 }
