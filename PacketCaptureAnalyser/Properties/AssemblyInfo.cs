c81d246bd36d7c1193d9cc3ea53da9db84cd5ba6
michael-mayes <michael-mayes@users.noreply.github.com>
2015-02-24 01:17:30 +0000

    Mark the assembly as not compliant with the Common Language Specification (CLS), as it makes use of unsigned integral types, to address a warning from FxCop

--- PacketCaptureAnalyser/Properties/AssemblyInfo.cs
+++ PacketCaptureAnalyser/Properties/AssemblyInfo.cs
@@ -23,6 +23,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// The assembly is not compliant with the Common Language
+// Specification (CLS) as it makes use of unsigned integral types
+[assembly: System.CLSCompliant(false)]
+
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
